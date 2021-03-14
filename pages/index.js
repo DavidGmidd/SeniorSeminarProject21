199d31ca2f8823e2c9e102787605e869cfe546eb
David G <dgikoshvili@middlebury.edu>
2021-03-14 13:54:49 -0400

    Run Dev working

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import { useState } from 'react';
+import Link from 'next/link';
+
+export default function Home() {
+
+
+
+return (
+    <div>
+<main>
+
+<div> HELLO WORLD </div>
+</main>
+
+<footer> CS 701 Final Project</footer>
+</div>
+
+);
+}
