79698f9532ab8bc80f22b518dd532b967377ca70
Thomas Tarantino <ttarantino@middlebury.edu>
2021-04-07 17:01:46 -0400

    redesigned front

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,25 +28,21 @@ export default function Home() {
     </Head>
     <main>
 
-      <div className={styles.welcome} >
-          <h2>Welcome to the Panther Crush</h2>
-      </div>
-          <div className={styles.intro} >
-              <h4>Graduation is coming too soon, so try to make the most of it while you can</h4>
-              <h4>See if your proc crush has also been crushing on you</h4> 
-          </div>
-          
-          <div className={styles.overview} >
-              <h3>How it works:</h3>
-          </div>
-  
-          <div className={styles.information} >
-              <p>We're taking the long-lived tradition of Crush Lists and making it more accessible for seniors</p>
-              <p> If you are a senior (class of 2021 and 2021.5), you can anonymously submit names of up to seven senior "crushes" between May 16 to May 21 by 5:00 pm </p>
-              <p> Privacy is important to us, so all of your personal information and the names you submit will be kept completely anonymous. Only if you match with your crush will your name be shared with them </p> 
-              <p>  You will get notified by email on May 22nd if you match with any of your crushes. After that, you can take it from there </p> 
-              <button className={styles.button} onClick={() => {setCurrentMode("CrushList")}}>Give me my crush</button>
-          </div>
+      <div className={styles.intro} >
+            <p>Graduation comes sooner than you think, so try to make the most of it while you can</p>
+        </div>
+        
+        <div className={styles.overview} >
+            <h3>How it works:</h3>
+        </div>
+
+        <div className={styles.information} >
+            <p>We're taking the long-lived tradition of Crush Lists and making it more accessible for seniors</p>
+            <p> If you are a senior (class of 2021 and 2021.5), you can anonymously submit names of up to seven senior "crushes" between May 16 to May 21 by 5:00 pm </p>
+            <p> Privacy is important to us, so all of your personal information and the names you submit will be kept completely anonymous. Only if you match with your crush will your name be shared with them </p> 
+            <p>  You will get notified by email on May 22nd if you match with any of your crushes. After that, you can take it from there </p> 
+            <button className={styles.button} onClick={() => {setCurrentMode("CrushList")}}>Give me my crush</button>
+        </div>
          
       </main> 
       </>) 
