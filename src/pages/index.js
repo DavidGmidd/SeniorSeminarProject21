b4034c6be70fb8072f09996de2abed6bf11f94a3
Thomas Tarantino <ttarantino@middlebury.edu>
2021-04-30 12:26:38 -0400

    add FAQ too home as test

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,6 +58,42 @@ export default function Home() {
       src="https://player.vimeo.com/video/538941352?background=1&controls=0"
       frameBorder="0" >
       </iframe>
+
+      <div className={styles.faqs} > 
+      <h2> FAQs</h2>
+
+      <details> 
+        <summary> Who is eligible? </summary>
+      <h4> Seniors in the class of 2021 and 2021.5. We will ask you to include your email when you submit your crushes to verify that you are a senior. </h4>
+      </details>
+
+      <details> 
+        <summary> When is this? </summary>
+      <h4> Seniors can fill out their virtual crush list between May 10 to May 17 by 5:00 pm. You will get notified if you match with any of your crushes on May 18. </h4>
+      </details>
+
+      <details> 
+        <summary> Who can see my crush list? </summary>
+      <h4> No one can see the list you submit. Only if you match with your crush will your name and email be shared with them.</h4>
+      </details>
+
+      <details> 
+        <summary> How do I submit my crushes? </summary>
+      <h4> Click on <b>My Crushes</b> at the bottom of the website to fill out your list. The form will ask you to include the email addresses of your crushes. </h4>
+      </details>
+
+      <details> 
+        <summary> How do I find my crush&apos;s email? </summary>
+      <h4> There are several ways you can find your crush&apos;s email. You can search their name in the directory, <a href= "https://directory.middlebury.edu/">found here</a>, or start typing their name into Outlook until their email appears.  </h4>
+      </details>
+
+      <details> 
+        <summary> What happens if I match with one of my crushes? </summary>
+      <h4> You will get notified by email on May 18. The email will include their name and email address for you to reach out to them, if you choose to do so.</h4>    
+      </details>
+
+    </div>
+
         <footer className={styles.footer}> 
           <button className={styles.button} onClick={() => {setCurrentMode("CrushList")}}>My Crushes</button>
         </footer>
