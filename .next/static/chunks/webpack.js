287acb2f965638510e1242daceaadd091706a456
Thomas Tarantino <ttarantino@middlebury.edu>
2021-04-30 10:48:24 -0400

    formatting

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2420465ff3402383a791";
+/******/ 	var hotCurrentHash = "1666d2955cd63cf7a83e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
